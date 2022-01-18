3badd1ec0a83900eaf58104ca783fc6e56c069c8
rainier letidjawa <rainierio@rainiers-MacBook-Pro-2.local>
2022-01-18 22:32:24 +0700

    disable unused morgan lib

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
-// const morgan = require("morgan");
+//const morgan = require("morgan");
 require("dotenv").config();
 
 // ----------------------------------
